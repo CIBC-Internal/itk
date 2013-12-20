f5e4311ad62168c29ad8f8f6b3f203d94a3b6b0f
Ayla Khan <ayla@sci.utah.edu>
2013-12-20 21:54:05 +0000

    Save clean copy of ITK v4.4.2.

---
+++ Modules/IO/DCMTK/itk-module.cmake
@@ -0,0 +1,14 @@
+set(DOCUMENTATION "This module contains the third party <a
+href=\"http://dicom.offis.de/dcmtk/\">DCMTK</a> DCMTK is a collection of libraries and applications implementing large parts the DICOM standard.")
+
+itk_module(ITKIODCMTK
+  DEPENDS
+    ITKDCMTK
+    ITKIOImageBase
+  TEST_DEPENDS
+    ITKTestKernel
+    ITKImageIntensity
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  EXCLUDE_FROM_ALL
+)
