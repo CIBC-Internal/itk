92d64207a667b7b21c8c21911f05e6692a8d6bcd
Ayla Khan <ayla@sci.utah.edu>
2014-06-09 23:33:13 +0000

    Move directory check to bash command.

--- CMake/itkExternal_FFTW.cmake
+++ CMake/itkExternal_FFTW.cmake
@@ -104,11 +104,9 @@ else()
     include_directories(${FFTW_INCLUDE_PATH})
     if(UNIX AND NOT APPLE)
       if(CMAKE_SIZEOF_VOID_P MATCHES 8)
-        if(EXISTS ${ITK_BINARY_DIR}/fftw/lib64)
-          add_custom_command(TARGET fftwd
-            POST_BUILD
-            COMMAND ln -sf ${ITK_BINARY_DIR}/fftw/lib64 ${ITK_BINARY_DIR}/fftw/lib)
-        endif()
+      add_custom_command(TARGET fftwd
+        POST_BUILD
+          COMMAND [ -d ${ITK_BINARY_DIR}/fftw/lib64 ] && ln -sf ${ITK_BINARY_DIR}/fftw/lib64 ${ITK_BINARY_DIR}/fftw/lib)
       endif()
     endif()
     #
